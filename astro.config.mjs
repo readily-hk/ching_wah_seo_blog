e0c20fcf3dd38db99cb80200c0148d00d0c72a24
Log G <15907288+logg926@users.noreply.github.com>
2024-10-12 17:56:57 +0800

    change base to blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://readily-hk.github.io",
-  base: "ching_wah_seo_blog",
+  base: "blog",
   // site: 'https://example.com',
   integrations: [mdx(), sitemap()],
 });
